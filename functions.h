c7055943a892b74666da5b152e2f2fc79e2b83fb
Arkneid <chaminejoao@gmail.com>
2024-09-06 20:10:01 +0100

    Issue #4 - Connection to authentication portal done

--- functions.h
+++ functions.h
@@ -7,5 +7,6 @@ int nc_strcmp(const char* s1, const char* s2);
 int nc_strlen(const char* str);
 void write_file(int nargs, char** args, const char* file_name);
 char* nc_strcpy(char* dst, const char* src);
+void write_file_portal(int nargs, char** args, const char* file_name);
 
 #endif
